842ba167e5b9a5497e620194dda833f6c8660523
sannendok <gremlenwow@yandex.ru>
2022-05-19 17:29:00 +0300

    Доделать функционал модального окна

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,13 @@
-let popup = document.querySelector('.popup');
-    popupOpenButton = document.querySelector('.profile__edit-button');
-    popupCloseButton = document.querySelector('.popup__close')
+let   popup = document.querySelector('.popup');
+let   popupOpenButton = document.querySelector('.profile__edit-button');
+let   popupCloseButton = document.querySelector('.popup__close')
+let   popInput = document.querySelector('.popup__input-name');
+let   profileName = document.querySelector('.profile__name');
+let   profileAbout = document.querySelector('.profile__description');
 
 function openPopup () {
     popup.classList.add('popup__open');
+    popInput.value = profileName.textContent;
 }
 function close (){
     popup.classList.remove('popup__open');
