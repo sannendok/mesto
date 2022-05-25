6a19d3c8dfd273464f50cd37e6803888664d485e
sannendok <gremlenwow@yandex.ru>
2022-05-25 12:52:34 +0300

    попытка 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,25 @@
 let popup = document.querySelector('.popup');
     popupOpenButton = document.querySelector('.profile__edit-button');
     popupCloseButton = document.querySelector('.popup__close')
-    popInputName = document.querySelector('.popup__input_name');
-    popInputDescription = document.querySelector('.popup__input_description');
+    popInputName = document.querySelector('.popup__input_value_name');
+    popInputDescription = document.querySelector('.popup__input_value_description');
     profileName = document.querySelector('.profile__name');
     profileDescription = document.querySelector('.profile__description');
     formElement = document.querySelector('.popup__form');
 
 function openPopup() {
-    popup.classList.add('popup__open');
+    popup.classList.add('popup_open');
     popInputName.value = profileName.textContent;
     popInputDescription.value = profileDescription.textContent;
 }
 function close() {
-    popup.classList.remove('popup__open');
+    popup.classList.remove('popup_open');
 }
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     profileName.textContent = popInputName.value;
     profileDescription.textContent = popInputDescription.value;
-    popup.classList.remove('popup__open');
+    popup.classList.remove('popup_open');
 }
 
 popupOpenButton.addEventListener('click', openPopup);
