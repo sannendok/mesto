0808e763bc7706fac60f43fa4f37dfb213aec882
sannendok <gremlenwow@yandex.ru>
2022-06-07 17:41:35 +0300

    закрытие и открытие

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,49 @@
-let popup = document.querySelector('.popup');
-    popupOpenButton = document.querySelector('.profile__edit-button');
-    popupCloseButton = document.querySelector('.popup__close')
-    popInputName = document.querySelector('.popup__input_value_name');
-    popInputDescription = document.querySelector('.popup__input_value_description');
-    profileName = document.querySelector('.profile__name');
-    profileDescription = document.querySelector('.profile__description');
-    formElement = document.querySelector('.popup__form');
 
-function openPopup() {
-    popup.classList.add('popup_open');
-    popInputName.value = profileName.textContent;
-    popInputDescription.value = profileDescription.textContent;
-}
-function close() {
-    popup.classList.remove('popup_open');
-}
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
+let popup = document.querySelector('.popup');
+popups = document.querySelectorAll('.popup');
+popupCard = document.querySelector('.card-popup');
+popupOpenButton = document.querySelector('.profile__edit-button');
+popupOpenAdd = document.querySelector('.profile__add-button')
+popupCloseButton = document.querySelector('.popup__close')
+popInputName = document.querySelector('.popup__input_value_name');
+popInputDescription = document.querySelector('.popup__input_value_description');
+profileName = document.querySelector('.profile__name');
+profileDescription = document.querySelector('.profile__description');
+formElement = document.querySelector('.popup__form');
+
+
+
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
     profileName.textContent = popInputName.value;
     profileDescription.textContent = popInputDescription.value;
     popup.classList.remove('popup_open');
 }
 
-popupOpenButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', close);
+
+const openPopup = (elem) => {
+    elem.classList.add('popup_open');
+}
+const close = (elem) => {
+    elem.classList.remove('popup_open');
+}
+// События
+popupOpenButton.addEventListener('click', () => {
+    openPopup(popup);
+    popInputName.value = profileName.textContent;
+    popInputDescription.value = profileDescription.textContent;
+});
+
+popupOpenAdd.addEventListener('click', () => {
+    openPopup(popupCard);
+});
+
+popups.forEach((popup) => {
+popup.addEventListener('click', e => {
+      if (e.target.classList.contains("popup__close")) {
+      close(popup);
+      }
+    });
+});
 formElement.addEventListener('submit', formSubmitHandler); 
